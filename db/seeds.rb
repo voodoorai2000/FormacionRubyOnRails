b2f628d0ee2501a300932c9c41fef105cc9d5e8a
rgarcia <voodoorai2000@gmail.com>
2014-10-11 17:56:49 +0200

    Adding city seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+names = ["Madrid", "Barcelona", "Valencia", "Sevilla", "Bilbao", "A Coruña", "Mallorca"]
+names.each { |name| City.create(name: name) }
